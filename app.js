d5d0f3b9890b777df066efd2a2a0159aede42f5d
Christian Thornton <gremke@gmail.com>
2024-05-28 21:34:16 -0700

    operator functions & other button functionality

--- app.js
+++ app.js
@@ -1,24 +1,91 @@
-const number = document.querySelectorAll('.number');
-let displayValue = document.getElementById('value');
-let value = 0;
-const clear = document.getElementById('clear');
-const clearDisplay = function () {
-  if (value === 0) return;
-    value = 0;
-    displayValue.textContent = 0;
-    console.log(value);
+let a = "";
+let b = "";
+let result = 0;
+let op = undefined;
+
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
+
+const evaluate = function (a, b, op) {
+  a = parseFloat(a);
+  b = parseFloat(b);
+  if (op === "add") return add(a, b);
+};
+
+function setOperator(operator) {
+  if (op && b) {
+    result = evaluate(a, b, op);
+    displayValue.textContent = result;
+    a = result;
+    b = "";
+    op = undefined;
+  }
+  op = operator;
+  console.log(`${operator}`);
 }
 
-number.forEach((number) => {
-  number.addEventListener('click', () => {
-    if (value === 0) displayValue.textContent = '';
-    value = displayValue.textContent += number.textContent;
-    console.log(parseInt(value))
-  })
+function resetOperation() {
+  a = "";
+  b = "";
+  result = 0;
+  op = undefined;
+  displayValue.textContent = "0";
+}
+
+const addButton = document.getElementById("add");
+const subtractButton = document.getElementById("subtract");
+const multiplyButton = document.getElementById("multiply");
+const divideButton = document.getElementById("divide");
+const equalsButton = document.getElementById("equals");
+const numbers = document.querySelectorAll(".number");
+let displayValue = document.getElementById("value");
+const clearButton = document.getElementById("clear");
+
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    if (op != undefined) {
+      displayValue.textContent = "";
+      b = b += number.textContent;
+    } else if (op === undefined) {
+      displayValue.textContent = "";
+      a = a += number.textContent;
+    } else if (result > 0 && op != undefined) {
+      displayValue.textContent = "";
+      b = b += number.textContent;
+    }
+    console.log(a, b);
+  });
 });
 
-clear.addEventListener('click', () => {
-  value = 0;
-  displayValue.innerHTML = 0;
-  console.log(value);
+clearButton.addEventListener("click", () => {
+  resetOperation();
+  console.log(a, b, op);
+});
+
+addButton.addEventListener("click", () => setOperator("add"));
+
+equalsButton.addEventListener("click", () => {
+  if (op && b) {
+    result = evaluate(a, b, op);
+    displayValue.textContent = result;
+    a = result;
+    b = "";
+    op = undefined;
+  }
+  //result = evaluate(a, b, op);
+  console.log("equals");
+  console.log(`${a} ${op} ${b} = ${result}`);
 });
